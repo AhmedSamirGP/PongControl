83d9bf4d81814c7a191ca5f7ef11c98dd95da357
Mohamed Saad <mohamedsaadamin.cis@gmail.com>
2021-02-18 23:48:08 +0200

    Edit AudioController

--- Assets/_PongControl/Scripts/AudioController.cs
+++ Assets/_PongControl/Scripts/AudioController.cs
@@ -3,24 +3,12 @@ public class AudioController : MonoBehaviour
 {
     [SerializeField]
     AudioSource hitAudioSource;
-    [SerializeField]
-    AudioSource themeAudioSource;
-
-    private void Awake()
-    {
-        PlayThemeClip();
-    }
 
     public void PlayHitSound()
     {
         hitAudioSource.Play();
     }
 
-
-    public void PlayThemeClip()
-    {
-        themeAudioSource.Play();
-    }
     public void PlaySound(AudioClip audioClip)
     {
         hitAudioSource.PlayOneShot(audioClip);
