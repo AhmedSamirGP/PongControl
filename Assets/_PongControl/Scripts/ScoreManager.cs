f8450a8806eb59cf91b276a693616b1aa2677635
Ahmed Samir <ahmedsamir.real.work@gmail.com>
2021-02-19 18:17:11 +0200

    Canvas Integrated

--- Assets/_PongControl/Scripts/ScoreManager.cs
+++ Assets/_PongControl/Scripts/ScoreManager.cs
@@ -9,12 +9,12 @@ public class ScoreManager: MonoBehaviour
     public TMP_Text text2;
     private int score = 0;
     private int score2 = 0;
-    private void OnIncreaseScore1()
+    public void OnIncreaseScore1()
     {
         score += 1;
         text1.text = score.ToString();
     }
-    private void OnIncreaseScore2()
+    public void OnIncreaseScore2()
     {
         score2 += 1;
         text2.text = score2.ToString();
